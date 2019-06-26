c2dec091c24e32d89673a21e46fff0692d98f416
Abdul Haseeb <38996347+rad1c4l@users.noreply.github.com>
2019-06-26 18:45:53 +0500

    Updates

---
+++ precache-manifest.fbc51395d6f6786bd383ba7918364ec4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb1d59b631f17d3053e78fa3ad2cb024",
+    "url": "/shapes-calculator/index.html"
+  },
+  {
+    "revision": "af9659f91e23d61d36e8",
+    "url": "/shapes-calculator/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "65667ab5d459d58c7b44",
+    "url": "/shapes-calculator/static/css/main.397bac47.chunk.css"
+  },
+  {
+    "revision": "af9659f91e23d61d36e8",
+    "url": "/shapes-calculator/static/js/2.e0e88dde.chunk.js"
+  },
+  {
+    "revision": "65667ab5d459d58c7b44",
+    "url": "/shapes-calculator/static/js/main.51edc3da.chunk.js"
+  },
+  {
+    "revision": "0f7f6b7570c1ff91b2a0",
+    "url": "/shapes-calculator/static/js/runtime~main.3fad441c.js"
+  },
+  {
+    "revision": "6d4e98c12edd8adaf4bc51d4ee7e6d15",
+    "url": "/shapes-calculator/static/media/BWLogo.6d4e98c1.png"
+  }
+]);
